f5ba6d3ceeaf517ee4869e4ce522707559c36663
Carlos Orellana <ce.orellanaf@gmail.com>
2019-08-18 16:33:00 -0500

    Agregados archivos para creacion de base de datos y procedures

---
+++ SQL Files/createProcedures.sql
@@ -0,0 +1,188 @@
+USE	DBTECNOIMPORT;
+GO
+
+CREATE PROCEDURE sp_agregar_cliente (
+@xml_datos XML,
+@err_code int output
+)
+AS
+BEGIN TRY
+	BEGIN TRANSACTION
+		INSERT INTO dbo.Cliente VALUES (
+			@xml_datos.value('(/Clientes/Cliente/cedula)[1]','varchar(10)'),
+			@xml_datos.value('(/Clientes/Cliente/nombre)[1]','varchar(255)'),
+			@xml_datos.value('(/Clientes/Cliente/direccion)[1]','varchar(255)'),
+			@xml_datos.value('(/Clientes/Cliente/telefono)[1]','varchar(15)')
+		);
+	COMMIT
+	SET @err_code = 0;
+END TRY
+BEGIN CATCH
+    ROLLBACK
+    SET @err_code = 1;
+END CATCH
+GO
+
+CREATE PROCEDURE sp_detalles_transaccion (
+@xml_datos_detalles XML,
+@id_transaccion int
+)
+AS
+	INSERT INTO dbo.Detalle_Transaccion (id_transaccion, id_articulo, cantidad) VALUES (
+		@id_transaccion,
+		@xml_datos_detalles.query('articulo').value('.','int'),
+		@xml_datos_detalles.query('cantidad').value('.','int')
+	);
+GO
+
+CREATE PROCEDURE sp_realizar_transaccion (
+@tipo VARCHAR(15),
+@xml_datos XML,
+@xml_datos_detalles XML,
+@err_code INT output
+)
+AS
+DECLARE @id_transaccion int;
+BEGIN TRY
+	BEGIN TRANSACTION
+		INSERT INTO dbo.Transaccion (id_cliente, id_establecimiento, fecha_creacion, hora_creacion, tipo, subtotal, total) VALUES (
+			@xml_datos.value('(/Transacciones/Transaccion/cedula)[1]','varchar(10)'),
+			@xml_datos.value('(/Transacciones/Transaccion/id_establecimiento)[1]','int'),
+			@xml_datos.value('(/Transacciones/Transaccion/fecha_creacion)[1]','varchar(8)'),
+			@xml_datos.value('(/Transacciones/Transaccion/hora_creacion)[1]','varchar(6)'),
+			@tipo,
+			@xml_datos.value('(/Transacciones/Transaccion/subtotal)[1]','float'),
+			@xml_datos.value('(/Transacciones/Transaccion/total)[1]','float')
+		);
+	SET @id_transaccion = SCOPE_IDENTITY();
+	EXEC sp_detalles_transaccion @xml_datos_detalles, @id_transaccion;
+	COMMIT
+	SET @err_code = 0;
+END TRY
+BEGIN CATCH
+    ROLLBACK
+    SET @err_code = 1;
+END CATCH
+GO
+
+CREATE PROCEDURE sp_buscar_cliente (
+@cedula VARCHAR(10)
+)
+AS
+	SELECT *
+	FROM dbo.Cliente
+	WHERE cedula = @cedula
+GO
+
+CREATE PROCEDURE sp_consultar_entrega (
+@fecha_inicio VARCHAR(8),
+@fecha_fin VARCHAR(8)
+)
+AS
+	SELECT *
+	FROM dbo.ENTREGA e
+	INNER JOIN dbo.Detalle_Entrega de
+	ON e.id_entrega = de.id_entrega
+	WHERE e.fecha_entrega BETWEEN 
+	@fecha_inicio AND @fecha_inicio
+GO
+
+CREATE PROCEDURE sp_buscar_articulo_nombre (
+@string VARCHAR(255)
+)
+AS
+	SELECT nombre, descripcion, categoria, precio
+	FROM dbo.Articulo
+	WHERE nombre LIKE @string
+GO
+
+CREATE PROCEDURE sp_buscar_articulo_descripcion (
+@string VARCHAR(255)
+)
+AS
+	SELECT nombre, descripcion, categoria, precio
+	FROM dbo.Articulo
+	WHERE descripcion LIKE @string
+GO
+
+CREATE PROCEDURE sp_buscar_articulo_categoria (
+@string VARCHAR(255)
+)
+AS
+	SELECT nombre, descripcion, categoria, precio
+	FROM dbo.Articulo
+	WHERE categoria LIKE @string
+GO
+
+CREATE PROCEDURE sp_entrega_no_asignada (
+@xml_datos_detalles XML,
+@err_code INT OUTPUT
+)
+AS
+BEGIN TRY
+	BEGIN TRANSACTION
+		INSERT INTO dbo.Detalle_Entrega (id_transaccion, detalle, fueExitosa) VALUES (
+			@xml_datos_detalles.value('(/Entregas/Entrega/transaccion)[1]','int'),
+			@xml_datos_detalles.value('(/Entregas/Entrega/detalles)[1]','varchar(255)'),
+			1
+		);
+	COMMIT
+	SET @err_code = 0;
+END TRY
+BEGIN CATCH
+    ROLLBACK
+    SET @err_code = 1;
+END CATCH
+GO
+
+CREATE PROCEDURE sp_asignar_repartidor (
+@id_entrega INT,
+@xml_entregas XML
+)
+AS
+	UPDATE dbo.Detalle_Entrega
+	SET id_entrega = @id_entrega
+	WHERE id_detalle =
+		(SELECT @xml_entregas.query('id_detalle').value('.','int'))
+GO
+
+CREATE PROCEDURE sp_crear_entrega (
+@xml_datos XML,
+@xml_entregas XML,
+@err_code INT OUTPUT
+)
+AS
+DECLARE @id_entrega int;
+BEGIN TRY
+	BEGIN TRANSACTION
+		INSERT INTO dbo.Entrega (id_repartidor, fecha_entrega, detalle_ruta) VALUES (
+			@xml_datos.value('(/Entregas/Entrega/id_repartidor)[1]','varchar(10)'),
+			@xml_datos.value('(/Entregas/Entrega/fecha_entrega)[1]','varchar(8)'),
+			@xml_datos.value('(/Entregas/Entrega/detalle_ruta)[1]','varchar(255)')
+		);
+	SET @id_entrega = SCOPE_IDENTITY();
+	EXEC sp_asignar_repartidor @id_entrega, @xml_entregas;
+	COMMIT
+	SET @err_code = 0;
+END TRY
+BEGIN CATCH
+    ROLLBACK
+    SET @err_code = 1;
+END CATCH
+GO
+
+CREATE PROCEDURE sp_iniciar_sesion (
+@usuario VARCHAR(255),
+@clave VARCHAR(255),
+@output BIT OUTPUT
+)
+AS
+SET @output = CASE WHEN EXISTS (
+    SELECT *
+    FROM dbo.Usuario
+    WHERE usuario = @usuario
+    AND clave = @clave
+	)
+THEN CAST(1 AS BIT)
+ELSE CAST(0 AS BIT) END
+GO
---
+++ SQL Files/createTecnoImport.sql
@@ -0,0 +1,153 @@
+CREATE DATABASE DBTECNOIMPORT
+USE DBTECNOIMPORT
+
+CREATE TABLE Articulo (
+	id_articulo int NOT NULL IDENTITY(1,1),
+	nombre varchar(255),
+	descripcion varchar(255),
+	categoria varchar(255),
+	precio float,
+	PRIMARY KEY (id_articulo)
+);
+
+CREATE TABLE Inventario (
+	id_inventario int NOT NULL IDENTITY(1,1),
+	id_articulo int,
+	cantidad int,
+	PRIMARY KEY (id_inventario),
+	FOREIGN KEY (id_articulo) REFERENCES Articulo(id_articulo)
+);
+
+CREATE TABLE Establecimiento (
+	id_establecimiento int NOT NULL IDENTITY(1,1),
+	direccion varchar(255),
+	telefono varchar(15),
+	tipo_establecimiento varchar(15),
+	id_inventario int,
+	PRIMARY KEY (id_establecimiento),
+	FOREIGN KEY (id_inventario) REFERENCES Inventario(id_inventario)
+);
+
+CREATE TABLE Empleado (
+	cedula varchar(10) NOT NULL,
+	nombre varchar(255),
+	fecha_nacimiento varchar(8),
+	correo varchar(255),
+	rol varchar(15),
+	id_establecimiento int,
+	PRIMARY KEY (cedula),
+	FOREIGN KEY (id_establecimiento) REFERENCES Establecimiento(id_establecimiento)
+);
+
+CREATE TABLE Usuario (
+	id_usuario int NOT NULL IDENTITY(1,1),
+	cedula varchar(10),
+	usuario varchar(255),
+	clave varchar(255),
+	PRIMARY KEY (id_usuario),
+	FOREIGN KEY (cedula) REFERENCES Empleado(cedula)
+);
+
+CREATE TABLE Compra_por_mayor (
+	id_compra_mayor int NOT NULL IDENTITY(1,1),
+	fecha_compra varchar(8),
+	hora_compra varchar(6),
+	total float,
+	detalles varchar(255),
+	PRIMARY KEY (id_compra_mayor)
+);
+
+CREATE TABLE Recibido_por_mayor (
+	id_recibido int NOT NULL IDENTITY(1,1),
+	id_compra_mayor int,
+	id_establecimiento int,
+	fecha_recibido varchar(8),
+	hora_recibido varchar(6),
+	PRIMARY KEY (id_recibido),
+	FOREIGN KEY (id_compra_mayor) REFERENCES Compra_por_mayor(id_compra_mayor)
+);
+
+CREATE TABLE Detalle_Recibido (
+	id_detalle int NOT NULL IDENTITY(1,1),
+	id_articulo int,
+	cantidad int,
+	PRIMARY KEY (id_detalle),
+	FOREIGN KEY (id_articulo) REFERENCES Articulo(id_articulo)
+);
+
+CREATE TABLE Cliente (
+	cedula varchar(10) NOT NULL,
+	nombre varchar(255),
+	direccion varchar(255),
+	telefono varchar(15),
+	PRIMARY KEY (cedula)
+);
+
+CREATE TABLE Transaccion (
+	id_transaccion int NOT NULL IDENTITY(1,1),
+	id_cliente varchar(10),
+	id_establecimiento int,
+	fecha_creacion varchar(8),
+	hora_creacion varchar(6),
+	tipo varchar(15),
+	subtotal float,
+	total float,
+	PRIMARY KEY (id_transaccion),
+	FOREIGN KEY (id_cliente) REFERENCES Cliente(cedula),
+	FOREIGN KEY (id_establecimiento) REFERENCES Establecimiento(id_establecimiento)
+);
+
+CREATE TABLE Detalle_Transaccion (
+	id_detalle int NOT NULL IDENTITY(1,1),
+	id_transaccion int,
+	id_articulo int,
+	cantidad int,
+	PRIMARY KEY (id_detalle),
+	FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion),
+	FOREIGN KEY (id_articulo) REFERENCES Articulo(id_articulo)
+);
+
+CREATE TABLE Abastecimiento (
+	id_abastecimiento int NOT NULL IDENTITY(1,1),
+	id_repartidor varchar(10),
+	fecha_envio varchar(8),
+	hora_envio varchar(6),
+	fecha_recibido varchar(8),
+	hora_recibido varchar(6),
+	id_bodega int,
+	id_local int,
+	PRIMARY KEY (id_abastecimiento),
+	FOREIGN KEY (id_bodega) REFERENCES Establecimiento(id_establecimiento),
+	FOREIGN KEY (id_local) REFERENCES Establecimiento(id_establecimiento),
+	FOREIGN KEY (id_repartidor) REFERENCES Empleado(cedula)
+);
+
+CREATE TABLE Detalle_Abastecimiento (
+	id_detalle int NOT NULL IDENTITY(1,1),
+	id_abastecimiento int,
+	id_articulo int,
+	cantidad int,
+	PRIMARY KEY (id_detalle),
+	FOREIGN KEY (id_abastecimiento) REFERENCES Abastecimiento(id_abastecimiento)
+);
+
+CREATE TABLE Entrega (
+	id_entrega int NOT NULL IDENTITY(1,1),
+	id_repartidor varchar(10),
+	fecha_entrega varchar(8),
+	detalle_ruta varchar(255),
+	PRIMARY KEY (id_entrega),
+	FOREIGN KEY (id_repartidor) REFERENCES Empleado(cedula)
+);
+
+CREATE TABLE Detalle_Entrega (
+	id_detalle int NOT NULL IDENTITY(1,1),
+	id_entrega int,
+	id_transaccion int,
+	hora_entrega varchar(6),
+	fueExitosa int,
+	detalle varchar(255),
+	PRIMARY KEY (id_detalle),
+	FOREIGN KEY (id_entrega) REFERENCES Entrega(id_entrega),
+	FOREIGN KEY (id_transaccion) REFERENCES Transaccion(id_transaccion)
+);
