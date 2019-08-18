60caee58c3a7295c2d86c516a77d5ec27243f092
Darjotri <darojos1997@gmail.com>
2019-08-17 19:18:00 -0500

    Cambios en paquetes

--- PrincipalTecnoImport/src/Principal/TecnoImporrt.java
+++ PrincipalTecnoImport/src/Principal/TecnoImporrt.java
@@ -3,19 +3,12 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package Vista;
+package Principal;
 
 /**
  *
  * @author Dario Triviño
  */
-public class PrincipalTecnoImport {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+public class TecnoImporrt {
     
 }
---
+++ PrincipalTecnoImport/src/Vista/VistaTecnoImport.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Vista;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author Dario Triviño
+ */
+public class VistaTecnoImport {
+
+    
+    
+}
