e0e346e3ea97744d926032838e470e8232f3dd27
Darjotri <45737453+Darjotri@users.noreply.github.com>
2019-08-23 09:39:39 -0500

    Renombrar paquetes

--- PrincipalTecnoImport/src/Modelo/Singleton/conexionsql.java
+++ PrincipalTecnoImport/src/Modelo/Singleton/conexionsql.java
@@ -1,9 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Modelo.Singleton;
+package modelo.singleton;
+
 import java.sql.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
