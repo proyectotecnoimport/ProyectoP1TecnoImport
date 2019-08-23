f8f68c6060887883d5f87b6bfae146f6674c4f9d
Darjotri <45737453+Darjotri@users.noreply.github.com>
2019-08-23 09:44:03 -0500

    Renombrar paquetes

--- PrincipalTecnoImport/src/Vista/Observer/VistaVendedor.java
+++ PrincipalTecnoImport/src/Vista/Observer/VistaVendedor.java
@@ -1,13 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Vista.Observer;
+package vista.observer;
 
-import Controlador.CtrlJefeBodega;
-import Controlador.CtrlVendedor;
-import Vista.VistaTecnoImport;
+import controlador.CtrlJefeBodega;
+import controlador.CtrlVendedor;
+import vista.VistaTecnoImport;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.geometry.Pos;
