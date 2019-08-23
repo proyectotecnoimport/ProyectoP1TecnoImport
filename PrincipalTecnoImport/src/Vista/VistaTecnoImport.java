a822655fec86ec17f958c2971d3e536a51a69875
Darjotri <45737453+Darjotri@users.noreply.github.com>
2019-08-23 09:44:42 -0500

    Renombrar paquetes

--- PrincipalTecnoImport/src/Vista/VistaTecnoImport.java
+++ PrincipalTecnoImport/src/Vista/VistaTecnoImport.java
@@ -1,11 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package Vista;
+package vista;
 
-import Controlador.CtrlSistema;
+import controlador.CtrlSistema;
 import javafx.scene.control.Button;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
